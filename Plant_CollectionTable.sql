f49a5ac179e0095d65d73574b36816e2fa0df081
mia-khan <141783751+mia-khan@users.noreply.github.com>
2024-03-03 01:18:46 -0800

    Update Plant_CollectionTable.sql

--- Plant_CollectionTable.sql
+++ Plant_CollectionTable.sql
@@ -1,7 +1,8 @@
 CREATE TABLE PlantCollections (
     collection_id INTEGER PRIMARY KEY,
     name TEXT,
-    description TEXT,
     user_id INTEGER,
+    plant_id INTEGER,
     FOREIGN KEY (user_id) REFERENCES Users(user_id)
+    FOREIGN KEY (plant_id) REFERENCES Plants(plant_id)
 );

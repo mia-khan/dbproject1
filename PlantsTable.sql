997f33ea8aa9eb6c21c4b9069d2925706d0eb2b7
mia-khan <141783751+mia-khan@users.noreply.github.com>
2024-03-03 01:16:16 -0800

    Update PlantsTable.sql

--- PlantsTable.sql
+++ PlantsTable.sql
@@ -1,9 +1,11 @@
 CREATE TABLE Plants (
-    plant_id INTEGER PRIMARY KEY,
-    name TEXT,
-    description TEXT,
-    watering_frequency TEXT,
-    humidity_exposure TEXT,
-    temperature_range TEXT,
-    date_added DATE
+plant_id INTEGER PRIMARY KEY,
+name TEXT,
+plant_family TEXT, 
+watering_frequency TEXT, 
+humidity_exposure TEXT, 
+temperature_range TEXT, 
+date_added DATE,
+user_id INTEGER, 
+FOREIGN KEY (user_id) REFERENCES Users(user_id) 
 );
